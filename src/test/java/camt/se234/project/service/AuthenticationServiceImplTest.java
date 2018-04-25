5ca95b040b680bd9fcaea9503783cdff03e4bc8d
Unknown <andrew.dopphoopha@gmail.com>
2018-04-25 15:06:12 +0700

    setup userDao
    setup userDao mock object in AuthenticationServiceImplTest class

--- src/test/java/camt/se234/project/service/AuthenticationServiceImplTest.java
+++ src/test/java/camt/se234/project/service/AuthenticationServiceImplTest.java
@@ -1,4 +1,30 @@
 package camt.se234.project.service;
 
+import camt.se234.project.dao.UserDao;
+import camt.se234.project.entity.User;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 public class AuthenticationServiceImplTest {
+    UserDao userDao;
+    AuthenticationServiceImpl authenticationService;
+
+    @Before
+    public void setup(){
+        userDao = mock(UserDao.class);
+        authenticationService = new AuthenticationServiceImpl();
+        authenticationService.setUserDao(userDao);
+    }
+
+    @Test
+    public void testAuthenicateWithMock(){
+        List<User> mockUsers = new ArrayList<>();
+
+    }
 }
