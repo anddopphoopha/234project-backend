059b05b6b5aa583b51e2c3eea481c04f893b23ef
anddopphoopha <andrew.dopphoopha@gmail.com>
2018-04-26 13:11:01 +0700

    implemented testAverageSaleOrderPrice method

--- src/test/java/camt/se234/project/service/SaleOrderServiceImplTest.java
+++ src/test/java/camt/se234/project/service/SaleOrderServiceImplTest.java
@@ -6,11 +6,12 @@ import camt.se234.project.entity.SaleOrder;
 import camt.se234.project.entity.SaleTransaction;
 import org.junit.Before;
 import org.junit.Test;
-
+import static org.hamcrest.Matchers.is;
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.closeTo;
 import static org.hamcrest.Matchers.hasItems;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -44,5 +45,16 @@ public class SaleOrderServiceImplTest {
         List<SaleTransaction> mockTransactions2 = new ArrayList<>();
         mockTransactions1.add(new SaleTransaction("T1", new SaleOrder("S1", mockTransactions1),
                 new Product("P1", "Carrot", "Can cause gout", "xxx1", 10.0), 10));
+        mockTransactions1.add(new SaleTransaction("T2", new SaleOrder("S1", mockTransactions1),
+                new Product("P2", "Meat", "Can cause gout", "xxx2", 50.0), 5));
+        mockTransactions2.add(new SaleTransaction("T3", new SaleOrder("S2", mockTransactions2),
+                new Product("P3", "IphoneX", "Can cause gout", "xxx3", 10.0), 10));
+        mockTransactions2.add(new SaleTransaction("T4", new SaleOrder("S2", mockTransactions2),
+                new Product("P4", "Banana", "is yellow", "xxx4", 100.0), 6));
+        List<SaleOrder> mockOrders = new ArrayList<>();
+        mockOrders.add(new SaleOrder("S1", mockTransactions1));
+        mockOrders.add(new SaleOrder("S2", mockTransactions2));
+        when(orderDao.getOrders()).thenReturn(mockOrders);
+        assertThat(saleOrderService.getAverageSaleOrderPrice(), is(closeTo(525.00, 0.01)));
     }
 }
